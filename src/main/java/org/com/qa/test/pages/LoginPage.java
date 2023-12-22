db312de7af4f7b2fe991bdbfc496b29967d42efe
AswinChandranPC <36229654+AswinChandranPC@users.noreply.github.com>
2023-12-22 03:18:58 -0800

    Update LoginPage.java
    added comments

--- src/main/java/org/com/qa/test/pages/LoginPage.java
+++ src/main/java/org/com/qa/test/pages/LoginPage.java
@@ -15,35 +15,51 @@ import java.util.regex.Pattern;
 public class LoginPage extends TestBase
 {
 
+    
+
     public LoginPage(){
         PageFactory.initElements(driver,this);
     }
 
+    //Object Repository of Login page
+    
+    //Username Text box
     @FindBy(name = "email")
     WebElement usernameTextBox;
-
+    
+    //Password Text box
     @FindBy(name = "password" )
     WebElement passwordTextBox;
 
+    //Login Button
     @FindBy(xpath = "//div[contains(text(),'Login')]")
     WebElement loginBtn;
 
+    //Home page Title
     @FindBy(xpath = "//div/span[contains(text(),'Aswin PC')]")
     WebElement homePageTitle;
 
+    //login test function
     public String loginTest(String un,String pwd) throws InterruptedException {
+        //Set username to username Text box
         usernameTextBox.sendKeys(un);
+        //Set password to password Text box
         passwordTextBox.sendKeys(pwd);
-
+        //Click on Login Button
         JavascriptExecutor js =(JavascriptExecutor)driver;
         js.executeScript("arguments[0].click()",loginBtn);
 
+        //Wait till the homePageTitle webelement text matches with expected pattern or text
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
         wait.until(ExpectedConditions.textMatches(By.xpath("//div/span[contains(text(),'Aswin PC')]"), Pattern.compile("Aswin PC")));
-
+        
+        //code for highlighting home page title
         JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
         jsExecutor. executeScript("arguments[0].setAttribute('style', 'border:2px solid red; background:yellow')", homePageTitle);
+        
         System.out.println(homePageTitle.getText());
+
+        //return home page title
         return homePageTitle.getText();
     }
 
